045ed72ca39d7a6d0acd4bb779129da3baf2bab7
shorgad <gad.shor@outlook.com>
2025-12-10 16:22:15 +0200

    Finish Demo

---
+++ Telhai.CS.CsharpCourse.DesignPatterns/CompositeDrawing/Test.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telhai.CS.CsharpCourse.DesignPatterns.CompositeDrawing
+{
+    public abstract class DrawingElement
+    {
+        protected string name;
+        // Constructor
+        public DrawingElement(string name)
+        {
+            this.name = name;
+        }
+        public abstract void Add(DrawingElement d);
+        public abstract void Remove(DrawingElement d);
+        public abstract void Display(int indent);
+    }
+    /// <summary>
+    /// The 'Leaf' class
+    /// </summary>
+    public class PrimitiveElement : DrawingElement
+    {
+        // Constructor
+        public PrimitiveElement(string name)
+            : base(name)
+        {
+        }
+        public override void Add(DrawingElement c)
+        {
+            Console.WriteLine(
+                "Cannot add to a PrimitiveElement");
+        }
+        public override void Remove(DrawingElement c)
+        {
+            Console.WriteLine(
+                "Cannot remove from a PrimitiveElement");
+        }
+        public override void Display(int indent)
+        {
+            Console.WriteLine(
+                new String('-', indent) + " " + name);
+        }
+    }
+    /// <summary>
+    /// The 'Composite' class
+    /// </summary>
+    public class CompositeElement : DrawingElement
+    {
+        List<DrawingElement> elements = new List<DrawingElement>();
+        // Constructor
+        public CompositeElement(string name)
+            : base(name)
+        {
+        }
+        public override void Add(DrawingElement d)
+        {
+            elements.Add(d);
+        }
+        public override void Remove(DrawingElement d)
+        {
+            elements.Remove(d);
+        }
+        public override void Display(int indent)
+        {
+            Console.WriteLine(new String('-', indent) +
+                "+ " + name);
+            // Display each child element on this node
+            foreach (DrawingElement d in elements)
+            {
+                d.Display(indent + 2);
+            }
+        }
+    }
+
+}
--- Telhai.CS.CsharpCourse.DesignPatterns/Program.cs
+++ Telhai.CS.CsharpCourse.DesignPatterns/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace Telhai.CS.CsharpCourse.DesignPatterns
+﻿using Telhai.CS.CsharpCourse.DesignPatterns.CompositeDrawing;
+using Telhai.CS.CsharpCourse.DesignPatterns.CompositePattern;
+
+namespace Telhai.CS.CsharpCourse.DesignPatterns
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Client client = new Client();
+
+            //// This way the client code can support the simple leaf
+            //// components...
+            //Leaf leaf = new Leaf();
+            //Console.WriteLine("Client: I get a simple component:");
+            //client.ClientCode(leaf);
+
+            //// ...as well as the complex composites.
+            //Composite tree = new Composite();
+            //Composite branch1 = new Composite();
+            //branch1.Add(new Leaf());
+            //branch1.Add(new Leaf());
+            //Composite branch2 = new Composite();
+            //branch2.Add(new Leaf());
+            //tree.Add(branch1);
+            //tree.Add(branch2);
+            //Console.WriteLine("Client: Now I've got a composite tree:");
+            //client.ClientCode(tree);
+
+            //Console.Write("Client: I don't need to check the components classes even when managing the tree:\n");
+            //client.ClientCode2(tree, leaf);
+
+            CompositeElement root = new CompositeElement("Picture");
+            root.Add(new PrimitiveElement("Red Line"));
+            root.Add(new PrimitiveElement("Blue Circle"));
+            root.Add(new PrimitiveElement("Green Box"));
+            // Create a branch
+            CompositeElement comp = new CompositeElement("Two Circles");
+            comp.Add(new PrimitiveElement("Black Circle"));
+            comp.Add(new PrimitiveElement("White Circle"));
+            root.Add(comp);
+            // Add and remove a PrimitiveElement
+            PrimitiveElement pe = new PrimitiveElement("Yellow Line");
+            root.Add(pe);
+            root.Remove(pe);
+            // Recursively display nodes
+            root.Display(1);
+            // Wait for user
+            Console.ReadKey();
+
+
         }
     }
 }
