146b8531a5d96e71f77a534d6b802c05b9b8367f
shorgad <gad.shor@outlook.com>
2025-12-10 15:19:00 +0200

    New Project Design Pattern Created

---
+++ Telhai.CS.CsharpCourse.DesignPatterns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Telhai.CS.CsharpCourse.DesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
